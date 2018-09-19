ce0ffca1eebc2eeb585c950ef20379558041e159
Jhonatas <costajhonatas9@gmail.com>
2018-09-19 13:40:00 -0300

    Add files via upload

---
+++ ParOuImpar/ParouImpar.c
@@ -0,0 +1,24 @@
+/*Escreva uma função em C que receba um número inteiro como parâmetro e retorne 1 se o
+número for par ou 0 se o número for ímpar.*/
+
+#include <stdio.h>
+
+int par, impar;
+int parOrImpar(int);
+
+int main(void){
+	int x;
+	printf("Digite um valor para saber se é par ou se é impar: ");
+	scanf("%d",&x);
+	parOrImpar(x);
+	printf("%d",parOrImpar(x));
+	return 0;
+}
+
+int parOrImpar(int x){
+	if (x%2==0){
+		return 1;
+	}else{
+		return 0;
+	}
+}
