f577d30fcef2de18afcecee97095255b5df47105
Jhonatas <costajhonatas9@gmail.com>
2018-09-24 14:08:15 -0300

    Add files via upload

--- CreatingVector/CreatingVector.c
+++ CreatingVector/CreatingVector.c
@@ -8,12 +8,17 @@ int main(void){
 	printf("Criando um vetor em C\n");
 	printf("Quantos valores quer digitar? (max. 10)\n");
 	scanf("%d", &t);
+	
+	for (int i = 0; i <10 ; i++){
+		vetor[i]=0;
+	}
+	
 	for (int i = 0; i < t; i++){
 		printf("Informe o  elemento %d: ", i+1);
 		scanf("%d", &vetor[i]);
 	}
 	printf("vetor preenchimento\n");
-	for (int i =0 ; i<t; i++){
+	for (int i =0 ; i<10; i++){
 		printf("%d\t", vetor[i]);
 	}
 	return 0;
