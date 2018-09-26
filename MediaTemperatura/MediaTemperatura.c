e8dfbf8579821ffa36d9a2c88a2cd7d942450188
Jhonatas <costajhonatas9@gmail.com>
2018-09-26 13:31:15 -0300

    Update MediaTemperatura.c

--- MediaTemperatura/MediaTemperatura.c
+++ MediaTemperatura/MediaTemperatura.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 #define N 1000
 
+void leTemperaturas(int,  float*);
+float calculaMedia (int, float*);
+float calculaVariancia(int, float*, float);
+
 int n;
-float *vetor[N], temp, med= 0.0f;
+float vetor[N], temp, med= 0.0f;
 
 int main(void){
-	
-	void leTemperaturas(int,  float*);
-	float calculaMedia (int, float*);
-	float calculaVariancia(int, float*, float);
-
 	printf("Quantos valores de temperatura vão ser inseridos (máx. 1000) ? ");
 	scanf("%d", &n);
 	
@@ -18,34 +17,33 @@ int main(void){
 		printf("Valor excede o limite");
 		return 1;
 	}
-	leTemperaturas(n,vetor[N]); 
+	leTemperaturas(n,vetor); 
 	
 	printf("vetor preenchimento\n");
 	for (int i =0 ; i<n; i++){
-		printf("%.2f", *vetor[i]);
+		printf("%.2f\t", vetor[i]);
 	}
+	calculaMedia(n, vetor);
 }
+
 	
 void leTemperaturas(int n,  float *v){
 	printf("Informe os valores coletados\n");
 	for (int i = 0; i < n; i++){
 		printf("Informe a temperatura %d ",i+1 );
-		scanf("%f", &*vetor[i]);
+		scanf("%f", &vetor[i]);
 	}	
 }
 
 float calculaMedia (int n, float *v){
 	for (int i= 0; i<n; i++){
-		printf("Temp %d:", i+1);
-		scanf("%f",&temp);
-		med+= temp;
+		med+=vetor[i];	
 	}
-	
 	med=med/n;
-	printf("Temperatura média: %.2f°c", med); 
+	printf("\nTemperatura média: %.2f°c", med); 
 	return 0;
 }
-/*
+
 float calculaVariancia(int n, float *v, float m){
 
-}*/
+}
